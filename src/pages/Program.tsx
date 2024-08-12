b13eddb1fce9cde580ddc82eb19fd5815190a500
Krisztian Bui <buikrisz@gmail.com>
2024-08-12 22:44:48 +0200

    program i18n

--- src/pages/Program.tsx
+++ src/pages/Program.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import timeline from "../assets/timeline.jpg";
+import timelineHU from "../assets/timeline_hu.jpg";
+import timelineEN from "../assets/timeline_en.jpg";
 import "./Program.css";
+import { useTranslation } from "react-i18next";
 
 export const Program = () => {
-  return (
-    <div id="program">
-      <img src={timeline} alt="Timeline" />
-    </div>
-  );
+  const { i18n } = useTranslation();
+  const lang = i18n?.language;
+
+  return <div id="program">{lang === "en" ? <img src={timelineEN} alt="Timeline" /> : <img src={timelineHU} alt="Timeline" />}</div>;
 };
